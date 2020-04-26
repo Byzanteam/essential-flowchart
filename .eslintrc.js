63053b18aa87c0de6cc6ee78690b4d3bd29fbbe7
Jiaxun Li <leecason0722@gmail.com>
2020-04-26 15:15:51 +0800

    🎉 initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,131 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        VariableDeclarator: {
+          var: 2,
+          let: 2,
+          const: 3,
+        },
+      },
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    quotes: [
+      'error',
+      'single',
+    ],
+    semi: [
+      'error',
+      'always',
+    ],
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    'max-len': [
+      'error',
+      {
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    'no-underscore-dangle': [
+      'off',
+    ],
+    'func-names': [
+      'error',
+      'as-needed',
+    ],
+    'no-multi-spaces': [
+      'error',
+      {
+        exceptions: {
+          VariableDeclarator: true,
+        },
+      },
+    ],
+    'one-var': [
+      'off',
+    ],
+    'no-param-reassign': [
+      'error',
+      {
+        props: false,
+      },
+    ],
+    'consistent-return': [
+      'off',
+    ],
+    'no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+      },
+    ],
+    radix: [
+      'off',
+    ],
+    'space-before-function-paren': [
+      'error',
+      'always',
+    ],
+    camelcase: [
+      'off',
+    ],
+    'function-paren-newline': [
+      'error',
+      'consistent',
+    ],
+
+    'import/no-unresolved': [
+      'off',
+    ],
+    'import/no-extraneous-dependencies': [
+      'off',
+    ],
+    'arrow-parens': [
+      'error',
+      'as-needed',
+    ],
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};

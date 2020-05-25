abb94fbbb571b352d5ab75582c5e04f958e10e5d
Phil Chen <06fahchen@gmail.com>
2020-05-25 14:36:16 +0800

    refactor: draw link

--- src/components/Link/utils/generatePath.ts
+++ src/components/Link/utils/generatePath.ts
@@ -1,37 +1,36 @@
 import PF from 'pathfinding';
-import store from '@/store';
-import { IPosition } from '@/types';
+import { IPosition, IGrid } from '@/types';
 import { SCALE_FACTOR } from '@/utils/grid';
+
+type Point = [number, number];
 
 const finder = PF.JumpPointFinder({
   heuristic: PF.Heuristic.manhattan,
   diagonalMovement: PF.DiagonalMovement.Never,
 });
 
-function generatePath (grid: PF.Grid, startPos: IPosition, endPos: IPosition): string {
-  const gridOffset = store.state.graph.grid.offset;
-  const startPosScaled = { x: Math.ceil((startPos.x + gridOffset.x) / SCALE_FACTOR), y: Math.ceil((startPos.y + gridOffset.y) / SCALE_FACTOR) };
-  const endPosScaled = { x: Math.ceil((endPos.x + gridOffset.x) / SCALE_FACTOR), y: Math.ceil((endPos.y + gridOffset.y) / SCALE_FACTOR) };
+export default function generatePath (
+  grid: IGrid,
+  startPos: IPosition,
+  endPos: IPosition,
+): Point[] {
+  const gridOffset = grid.offset;
+  const scaledStartPos = {
+    x: Math.ceil((startPos.x + gridOffset.x) / SCALE_FACTOR),
+    y: Math.ceil((startPos.y + gridOffset.y) / SCALE_FACTOR),
+  };
+  const scaledEndPos = {
+    x: Math.ceil((endPos.x + gridOffset.x) / SCALE_FACTOR),
+    y: Math.ceil((endPos.y + gridOffset.y) / SCALE_FACTOR),
+  };
 
-  const path = PF.Util.compressPath(
+  return PF.Util.compressPath(
     finder.findPath(
-      startPosScaled.x,
-      startPosScaled.y,
-      endPosScaled.x,
-      endPosScaled.y,
-      grid.clone(),
+      scaledStartPos.x,
+      scaledStartPos.y,
+      scaledEndPos.x,
+      scaledEndPos.y,
+      grid.pfGrid.clone(),
     ),
-  );
-
-  // TODO: another path
-  if (!path.length) return '';
-
-  const [first, ...rest] = path;
-  let d = `M${first[0] * SCALE_FACTOR - gridOffset.x} ${first[1] * SCALE_FACTOR - gridOffset.y}`;
-  rest.forEach(([x, y]) => {
-    d += ` L${x * SCALE_FACTOR - gridOffset.x} ${y * SCALE_FACTOR - gridOffset.y}`;
-  });
-  return d;
+  ) as Point[];
 }
-
-export default generatePath;

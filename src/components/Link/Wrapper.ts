465114fd98c2dc60c3f52db2e6beac382e659958
Phil Chen <06fahchen@gmail.com>
2020-05-28 18:45:41 +0800

    feat: consider nodePadding when generateRightAnglePath

--- src/components/Link/Wrapper.ts
+++ src/components/Link/Wrapper.ts
@@ -30,21 +30,25 @@ export default defineComponent({
 
     const fromNode = computed(() => graph.value.nodes[props.link.from.nodeId]);
 
-    const startPos = computed(() => fromNode.value.ports[props.link.from.portId].position);
-    const endPos = computed(() => {
+    const startPort = computed(() => fromNode.value.ports[props.link.from.portId]);
+    const endPort = computed(() => {
       if (props.link.to.nodeId && props.link.to.portId) {
         const toNode = graph.value.nodes[props.link.to.nodeId];
-        return toNode.ports[props.link.to.portId].position;
+        return toNode.ports[props.link.to.portId];
       }
-      return store.state.mousePosition;
+      if (store.state.mousePosition) {
+        return {
+          position: store.state.mousePosition,
+        };
+      }
     });
 
     const path = computed(() => {
-      if (endPos.value) {
+      if (endPort.value) {
         return generatePath(
           graph.value.grid,
-          startPos.value,
-          endPos.value,
+          startPort.value,
+          endPort.value,
           // track change
           store.state.linkVersions[props.link.id],
         );
@@ -58,13 +62,13 @@ export default defineComponent({
     ));
 
     return () => {
-      if (endPos.value) {
+      if (endPort.value) {
         return createElement(props.linkComponent, {
           props: {
             link: props.link,
 
-            startPos: startPos.value,
-            endPos: endPos.value,
+            startPos: startPort.value.position,
+            endPos: endPort.value.position,
 
             path: path.value,
           },
--- src/components/Link/utils/generatePath.ts
+++ src/components/Link/utils/generatePath.ts
@@ -1,28 +1,81 @@
 import PF from 'pathfinding';
-import { Point, IPosition, IGrid } from '@/types';
+import {
+  Point, IPosition,
+  PortDirection,
+  IGrid, INodePort,
+} from '@/types';
 import { pathFinder, SCALE_FACTOR } from '@/utils/grid';
 
 import generateRightAnglePath from './generateRightAnglePath';
+
+type NodePort = Pick<INodePort, 'position'> & Partial<Omit<INodePort, 'position'>>;
 
 function scalePath (path: Point[]): Point[] {
   return path.map(([x, y]) => [x * SCALE_FACTOR, y * SCALE_FACTOR]);
 }
 
+function scalePosition (position: IPosition): IPosition {
+  return {
+    x: Math.ceil(position.x / SCALE_FACTOR),
+    y: Math.ceil(position.y / SCALE_FACTOR),
+  };
+}
+
+// TODO:
+const PADDING = 10;
+function getPaddingPoint ({ position, direction }: NodePort): IPosition {
+  switch (direction) {
+    case PortDirection.TOP:
+      return { ...position, y: position.y - PADDING };
+      break;
+
+    case PortDirection.RIGHT:
+      return { ...position, x: position.x + PADDING };
+      break;
+
+    case PortDirection.BOTTOM:
+      return { ...position, y: position.y + PADDING };
+      break;
+
+    case PortDirection.LEFT:
+      return { ...position, x: position.x - PADDING };
+      break;
+
+    default:
+      return { ...position };
+  }
+}
+
+function fallbackPath (startPort: NodePort, endPort: NodePort): Point[] {
+  const scaledStartPos = scalePosition(getPaddingPoint(startPort));
+  const scaledEndPos = scalePosition(getPaddingPoint(endPort));
+
+  return scalePath([
+    [startPort.position.x, startPort.position.y],
+    ...generateRightAnglePath(scaledStartPos, scaledEndPos),
+    [endPort.position.x, endPort.position.y],
+  ]);
+}
+
 export default function generatePath (
   grid: IGrid,
-  startPos: IPosition,
-  endPos: IPosition,
+  startPort: NodePort,
+  endPort: NodePort,
   _version?: number,
 ): Point[] {
   const gridOffset = grid.offset;
-  const scaledStartPos = {
-    x: Math.ceil((startPos.x + gridOffset.x) / SCALE_FACTOR),
-    y: Math.ceil((startPos.y + gridOffset.y) / SCALE_FACTOR),
-  };
-  const scaledEndPos = {
-    x: Math.ceil((endPos.x + gridOffset.x) / SCALE_FACTOR),
-    y: Math.ceil((endPos.y + gridOffset.y) / SCALE_FACTOR),
-  };
+
+  const startPos = startPort.position;
+  const endPos = endPort.position;
+
+  const scaledStartPos = scalePosition({
+    x: startPos.x + gridOffset.x,
+    y: startPos.y + gridOffset.y,
+  });
+  const scaledEndPos = scalePosition({
+    x: endPos.x + gridOffset.x,
+    y: endPos.y + gridOffset.y,
+  });
 
   try {
     const path = PF.Util.compressPath(
@@ -35,10 +88,10 @@ export default function generatePath (
       ),
     ) as Point[];
 
-    if (!path.length) return scalePath(generateRightAnglePath(scaledStartPos, scaledEndPos));
+    if (!path.length) return fallbackPath(startPort, endPort);
 
     return scalePath(path);
   } catch (e) {
-    return scalePath(generateRightAnglePath(scaledStartPos, scaledEndPos));
+    return fallbackPath(startPort, endPort);
   }
 }

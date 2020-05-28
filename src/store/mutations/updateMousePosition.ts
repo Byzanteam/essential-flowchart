1ffd772c5e87c6ae8443956b330689c0afce8e06
Phil Chen <06fahchen@gmail.com>
2020-05-28 22:08:17 +0800

    todo: expand grid when mouse moving

--- src/store/mutations/updateMousePosition.ts
+++ src/store/mutations/updateMousePosition.ts
@@ -1,5 +1,6 @@
 import { IState, IPosition } from '@/types';
 
 export default function updateMousePosition (state: IState, position: IPosition) {
+  // TODO: expand grid
   state.mousePosition = position;
 }

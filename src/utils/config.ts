8ed2805c9cb581087dfc5c02323426a6ef25a869
Phil Chen <06fahchen@gmail.com>
2020-06-02 23:26:54 +0800

    feat: validate the config input

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
-import { IConfigInput, IConfig } from '@/types';
+import { IConfigInput, IConfig, ILinkPipelinePhase } from '@/types';
 import generateLinkId from '@/pipelines/generateLinkId';
 import distinctFromNodeAndToNode from '@/pipelines/distinctFromNodeAndToNode';
 
@@ -7,6 +7,26 @@ export const SCALE_FACTOR = 5;
 const DEFAULT_NODE_PADDING = SCALE_FACTOR * 2;
 const DEFAULT_PORT_GAP = SCALE_FACTOR * 2;
 
+function isPositiveInteger (input: number): boolean {
+  return Number.isInteger(input) && input > 0;
+}
+
+function validateNodePadding (nodePadding: number) {
+  if (!isPositiveInteger(nodePadding)) {
+    throw new Error(`nodePadding must be a positive integer, but got ${JSON.stringify(nodePadding)}.`);
+  }
+}
+
+function validatePortGap (portGap: number) {
+  if (!isPositiveInteger(portGap)) {
+    throw new Error(`portGap must be a positive integer, but got ${JSON.stringify(portGap)}.`);
+  }
+}
+
+function validateLinkPipeline (linkPipeline: ILinkPipelinePhase[]) {
+  if (!Array.isArray(linkPipeline)) throw new Error('');
+}
+
 export function buildConfig (
   {
     nodePadding = DEFAULT_NODE_PADDING,
@@ -14,6 +34,10 @@ export function buildConfig (
     linkPipeline = [generateLinkId, distinctFromNodeAndToNode],
   }: IConfigInput,
 ): IConfig {
+  validateNodePadding(nodePadding);
+  validatePortGap(portGap);
+  validateLinkPipeline(linkPipeline);
+
   return {
     nodePadding,
     portGap,

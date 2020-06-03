cfd1f8540fe5e8c2ee456190a463445e54ce3acc
Phil Chen <06fahchen@gmail.com>
2020-06-03 08:59:51 +0800

    feat: nodePadding and portGap can be zero

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -7,18 +7,18 @@ export const SCALE_FACTOR = 5;
 const DEFAULT_NODE_PADDING = SCALE_FACTOR * 2;
 const DEFAULT_PORT_GAP = SCALE_FACTOR * 2;
 
-function isPositiveInteger (input: number): boolean {
+function isNonNegativeInteger (input: number): boolean {
   return Number.isInteger(input) && input > 0;
 }
 
 function validateNodePadding (nodePadding: number) {
-  if (!isPositiveInteger(nodePadding)) {
+  if (!isNonNegativeInteger(nodePadding)) {
     throw new Error(`nodePadding must be a positive integer, but got ${JSON.stringify(nodePadding)}.`);
   }
 }
 
 function validatePortGap (portGap: number) {
-  if (!isPositiveInteger(portGap)) {
+  if (!isNonNegativeInteger(portGap)) {
     throw new Error(`portGap must be a positive integer, but got ${JSON.stringify(portGap)}.`);
   }
 }

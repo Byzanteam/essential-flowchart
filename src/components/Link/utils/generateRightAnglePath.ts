4584e622f8066420e16f79bf7beeb3e6b92d7d66
moonRider <solar.pom@gmail.com>
2020-06-16 15:00:07 +0800

    doc: update generateRightAnglePath notation
    - describe algorithm logic

--- src/components/Link/utils/generateRightAnglePath.ts
+++ src/components/Link/utils/generateRightAnglePath.ts
@@ -1,7 +1,6 @@
 import { IPosition, Point2D, IVector } from '@/types';
 
-/** ************************************
- * Start higher than End
+/**
  * Start      F          A
  * ----------------------
  * |                    |
@@ -9,58 +8,28 @@ import { IPosition, Point2D, IVector } from '@/types';
  * |                    |
  * ---------------------- End
  * B          C
- ******************************
- * A          F          Start
- * ----------------------
- * |                    |
- * |D                   |E
- * |                    |
- * ---------------------- B
- * End        C
  *
- **************************************
- * Start lower than End
- * B          C          End
- * ----------------------
- * |                    |
- * |E                   |D
- * |                    |
- * ---------------------- A
- * Start      F
+ * A: A.x = End.x, A.y = Start.y
+ * B: B.x = Start.x, B.y = End.y
+ * C: midpoint of B and End
+ * D: midpoint of A and End
+ * E: midpoint of B and Start
+ * F: midpoint of A and Start
  *
- **************************************
- *
- * End        C          B
- * ----------------------
- * |                    |
- * |D                   |E
- * |                    |
- * ---------------------- Start
- * A          F
- *
- **************************************
- * so that next point can be A, F(FC), E(ED), B
- * if startDirection // (start -> A), and with reverse direction
- * so that next point can not be A, F(FC)
- * ************************************
- * vectorS: OStrat -> Start
- * vectorE: End -> OEnd
- *
- *    1        2              3
- *  Start    OStart    OStart - Start
- *    |        |
- *  OStart   Start     Start - OStart
- *                            4
- **************************************
+ * when a path from Start to End,
+ * so that next point can be A, F(FC), E(ED), B.
+ * but if start with a direction(originalStart -> Start),
+ * and the direction is reverse to Start -> A,
+ * so that next point can not be A, F(FC)。
+ * and same on End side
  */
 
-const VectorMagnitude = (vector: IVector): number => Math.sqrt((vector[0] ** 2) + (vector[1] ** 2));
-
+// convert two point to a vector
 const Vector = (p1: IPosition, p2: IPosition): IVector => [p2.x - p1.x, p2.y - p1.y];
-
+// get vector magnitude(length)
+const VectorMagnitude = (vector: IVector): number => Math.sqrt((vector[0] ** 2) + (vector[1] ** 2));
 // is vector1 and vector2 parallel
 const isParallel = (v1: IVector, v2: IVector): boolean => v1[0] * v2[1] === v1[1] * v2[0];
-
 // is vector1 and vector2 with reverse direction
 const isReverseDirection = (v1: IVector, v2: IVector): boolean => isParallel(v1, v2)
     && VectorMagnitude(v1) > 0
@@ -85,15 +54,22 @@ export default function generateRightAnglePath (
         PFC: [IPosition, IPosition] = [PF, PC],
         // PE and PD can not stand alone
         PED: [IPosition, IPosition] = [PE, PD],
+        // originalStart -> Start
         vectorS: IVector = Vector(originalStart, start),
+        // End -> originalEnd, can be zero vector if end === originalEnd
         vectorE: IVector = Vector(end, originalEnd),
         // vector:  Start -> PB
         S_PB: IVector = Vector(start, PB),
         // vector:  Start -> PA
         S_PA: IVector = Vector(start, PA),
+        // PB -> End
         PB_E: IVector = Vector(PB, end),
+        // PA -> End
         PA_E: IVector = Vector(PA, end);
+
+  // possible next points after Start
   let allowedPoints = [PA, PB, PFC, PED];
+
   // limit by start point and start direction
   if (isReverseDirection(S_PA, vectorS)) {
     allowedPoints = [PB, PED];
@@ -107,7 +83,8 @@ export default function generateRightAnglePath (
   } else if (isReverseDirection(PB_E, vectorE)) {
     allowedPoints = allowedPoints.filter(point => [PA, PED].includes(point));
   }
-  // if next point has multiple choices, choose first choice
+  // if next point has multiple choices, choose the first choice
+  // TODO: Optimization choose strategy
   const path: IPosition | [IPosition, IPosition] = allowedPoints[0];
   const pathPoints: Point2D[] = Array.isArray(path)
     ? path.map(position => [position.x, position.y])

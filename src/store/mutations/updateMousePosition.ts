5d87a7d8851064de7e2a3f3cc72f3f805293954e
leecason <leecason0722@gmail.com>
2020-06-29 20:35:28 +0800

    improve: 🎨 remove grid offset and comment

--- src/store/mutations/updateMousePosition.ts
+++ src/store/mutations/updateMousePosition.ts
@@ -1,6 +1,5 @@
 import { IState, IPosition } from '@/types';
 
 export default function updateMousePosition (state: IState, position: IPosition) {
-  // TODO: expand grid
   state.mousePosition = position;
 }
